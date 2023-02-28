db2c12a7c98ce610ea888f1e80c17ad1b2854ef0
Steve <kornilov.stepan11.s06@gmail.com>
2023-02-28 13:49:13 +0200

    Update data

--- archivator.py
+++ archivator.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+import glob
 
 
 def screen() -> None:
@@ -9,7 +10,15 @@ def screen() -> None:
     date = '-'.join(map(str, [json_obj['day'], json_obj['month'], json_obj['year']]))
 
     if os.path.exists(f"exel_archive/{date}.xlsx"):
-        pass
+        os.mkdir(f"{date}")
+        shutil.move(f"exel_archive/{date}.xlsx", f"exel_archive/{date}/{date}.xlsx")
+        os.rename(f"exel_archive/{date}/{date}.xlsx",
+                  f"exel_archive/{date}/1.xlsx")
+
+    elif os.path.exists(f"exel_archive/{date}"):
+        shutil.copy2("m2023.xlsx", f"exel_archive/{date}")
+        os.rename("exel_archive/m2023.xlsx", f"exel_archive/{len(glob.glob('/exel_archive/*')) + 1}.xlsx")
+
     else:
         shutil.copy2("m2023.xlsx", "exel_archive")
         os.rename("exel_archive/m2023.xlsx",
