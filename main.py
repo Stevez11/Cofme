575e00fe4f546834412bb707207220cbe8f80ce3
Steve <kornilov.stepan11.s06@gmail.com>
2023-03-04 20:33:31 +0200

    corrected for tg bot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import request, exel_filler
+from cofme.project.mining_income_statistics import exel_filler, req
 import json
 
 
@@ -6,11 +6,7 @@ def start():
     with open('../../private/hashrate_on_triple_minning.json') as f:
         json_obj = json.load(f)
 
-    data = request.get_data(ethash_hash_rate=json_obj["ethash"],
-                            kas_hash_rate=json_obj["kas"],
-                            consumption=json_obj["consumption"])
+    data = req.get_data(ethash_hash_rate=json_obj["ethash"],
+                        kas_hash_rate=json_obj["kas"],
+                        consumption=json_obj["consumption"])
     exel_filler.update_data(data)
-
-
-if __name__ == "__main__":
-    start()
