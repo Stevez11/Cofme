f88cd62e58251722a2264736aa2b32605c7837a3
Steve <kornilov.stepan11.s06@gmail.com>
2023-03-05 14:46:47 +0200

    debug for tg bot

--- src/core/archivator.py
+++ src/core/archivator.py
@@ -6,11 +6,9 @@ import shutil
 def screen() -> None:
     with open("../../private/last_update.json") as f:
         json_obj = json.load(f)
+
     date = '-'.join(map(str, [json_obj['day'], json_obj['month'], json_obj['year']]))
 
-    if os.path.exists(f"../../private/exel_archive/{date}.xlsx"):
-        os.remove(f"../../private/exel_archive/{date}.xlsx")
-
-    shutil.copy2("../../private/m2023.xlsx", "../../private/exel_archive")
-    os.rename("../../private/exel_archive/m2023.xlsx",
-              f"../../private/exel_archive/{date}.xlsx")
+    os.rename("../../private/m2023.xlsx", f"../../private/{date}.xlsx")
+    shutil.copy2(f"../../private/{date}.xlsx", f"../../private/exel_archive/{date}.xlsx")
+    os.remove(f"../../private/{date}.xlsx")
